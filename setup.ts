0a799e5ac26c4a9f9a549043e254c4a4ebe337be
alephao <7674479+alephao@users.noreply.github.com>
2022-03-31 23:49:54 -0300

    feat: add oddworx functions

--- setup.ts
+++ setup.ts
@@ -1,11 +1,19 @@
 import { wallets } from "./scripts/wallets";
 import { deployAll } from "./scripts/deploy";
 import * as Genzee from "./scripts/genzee";
+import * as Oddworx from "./scripts/oddworx"
+import { contractAddress } from "./scripts/common";
+import { parseEther } from "ethers/lib/utils";
 
 const main = async () => {
   await deployAll();
+  
   await Genzee.startSale();
   await Genzee.mint(wallets[1], 20);
+
+  await Oddworx.toggleAdmin(contractAddress.staking);
+  await Oddworx.mint(wallets[1].address, parseEther("900"))
+
 };
 
 main();
